752fb766b95cb69a1e9e8504c0034048377b8b0f
Matthew OTousa <mlotousa@gmail.com>
2019-10-22 15:35:13 -0700

    done

--- calculator.js
+++
@@ -1,4 +0,0 @@
-let ds = document.querySelector.bind(document), l=ds('#l'), o=ds('#o'), r=ds('#r'), ch='change',
-    c = () => ds('#out').innerHTML = '' + eval(l.value + o.options[o.selectedIndex].text + r.value)
-l.addEventListener(ch, c); r.addEventListener(ch, c); o.addEventListener(ch, c); c()
-
